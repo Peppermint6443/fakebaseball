9e8aaf51e0952c826985ea01b36cc20fa802ebcf
Ethan Thompson <ethanlt6443.pie@gmail.com>
2025-11-19 17:45:43 -0600

    initial build

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # fakebaseball
+
+
+
+
+
+# CS 260 Topics
+* Web server setup
+* HTML
+* CSS
+* JavaScript and Web Frameworks
+    * HTML/CSS components and routing
+    * reactivity
+* HTTP service
+* data and authentication services
+* websocket
+* Security
+
+# EE 301 Topics
+## Class Topics
+
+
+3) diodes, LEDs
+4) microcontrollers, Arduino
+5) number systems
+6) state machines
+7) voltage and current dividers
+8) thevenin
+9) batteries
+10) node-voltage
+11) R-2R ladders
+12) inductors and capacitors
+13) first order circuits
+14) ADC
+15) PWM and DAC
+16) op amps
+17) op amps and photo diodes
+18) transistors
+19) high current amplification
+20) DC motors
+21) H-bridge stepper motors
+22) power supplies
+
+24) sensors
+25) serial communication
+26) interfacing sensors to Arduino
+27) sinusoidal sources, complex numbers
+28) phasors
+29) transfer functions
+30) filters and bode plot
+31) filter examples
+
+## Lab Topics
+1) intro to Arduino
+2) State Machine
+3) basic robot
+4) capacitive sensor and variable motor control
+5) light direction finder
+6) light following robot
+7) inventor's fair
---
+++ src/app.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+// import Home from './components/Home';
+// import Player from './components/Player';
+
+import './app.css';
+
+export default function App() {
+
+    return (
+        <BrowserRouter>
+            <div className = 'body'>
+                <header className = 'header'>
+                    <h1><NavLink className = 'nav-link' to = ''><p className = 'header-text'>Snek Scouting</p></NavLink></h1>
+
+                    <nav className = 'nav'>
+                        <p><NavLink className = 'NavLink' to = ''>Home</NavLink></p>
+                    </nav>
+                </header>
+
+                {/* <Routes>
+                    <Route 
+                        path = '/'
+                        element = {<Home/>}
+                    />
+                    <Route path = '/player_page' element = {<Player/>}/>
+                </Routes> */}
+
+                <footer>
+                    <span>Author: u/shruggingemoji64 (Reddit), Silicon1464 (Discord)</span>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+}
